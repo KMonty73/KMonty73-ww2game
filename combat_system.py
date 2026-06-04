1cc6a448f52a1e4a2b1f89ac226d1c5f68ab4e97
KMonty73 <137738741+KMonty73@users.noreply.github.com>
2026-06-04 07:39:30 -0400

    Create combat_system.py

---
+++ combat_system.py
@@ -0,0 +1,17 @@
+def attack_city():
+
+    print("\nGENERAL LEVEL")
+    print("-------------------")
+    print("Attack ordered on Smolensk.")
+
+    print("\nMAJOR LEVEL")
+    print("-------------------")
+    print("Urban assault sectors assigned.")
+
+    print("\nCAPTAIN LEVEL")
+    print("-------------------")
+    print("Infantry and armor advancing.")
+
+    print("\nINDIVIDUAL COMBAT")
+    print("-------------------")
+    print("Battle begins inside city.")
