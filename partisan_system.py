21d8842d85c936899e1f22dcb68f9226d0dcaa9e
KMonty73 <137738741+KMonty73@users.noreply.github.com>
2026-06-04 07:40:45 -0400

    Create partisan_system.py

---
+++ partisan_system.py
@@ -0,0 +1,8 @@
+def partisan_activity():
+
+    print("\nPARTISAN ACTIVITY")
+    print("-------------------")
+    print("Rail sabotage reported.")
+    print("Supply delivery delayed.")
+    print("Bridge damage detected.")
+    print("Enemy logistics disrupted.")
