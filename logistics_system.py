0ebeaf9a850aa033ddf554500b5cfe8621b42c57
KMonty73 <137738741+KMonty73@users.noreply.github.com>
2026-06-04 07:40:09 -0400

    Create logistics_system.py

---
+++ logistics_system.py
@@ -0,0 +1,9 @@
+def supply_report():
+
+    print("\nLOGISTICS REPORT")
+    print("-------------------")
+    print("Rail hubs operational.")
+    print("Supply depots advancing.")
+    print("Fuel reserves stable.")
+    print("Truck convoys active.")
+    print("Partisan activity may disrupt deliveries.")
