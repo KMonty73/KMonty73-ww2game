bd2a93a7dc30cf12067ca7418a3797c164ce6378
KMonty73 <137738741+KMonty73@users.noreply.github.com>
2026-06-04 07:29:22 -0400

    Create intel_system.py

---
+++ intel_system.py
@@ -0,0 +1,8 @@
+def show_intelligence():
+
+    print("\nINTELLIGENCE REPORT")
+    print("-------------------")
+    print("Heavy radio traffic detected.")
+    print("Possible armored movement.")
+    print("Rail activity increasing.")
+    print("Enemy buildup near Smolensk.")
