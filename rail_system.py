67022fc2ece92b576f1e7bb34b752281e03e6de6
KMonty73 <137738741+KMonty73@users.noreply.github.com>
2026-06-04 07:51:05 -0400

    Create rail_system.py

---
+++ rail_system.py
@@ -0,0 +1,9 @@
+def rail_network_status():
+
+    print("\nRAIL NETWORK STATUS")
+    print("-------------------")
+    print("Primary rail hubs operational.")
+    print("Minor rail damage reported.")
+    print("Engineers conducting automatic repairs.")
+    print("Supply trains moving toward frontline depots.")
+    print("Partisan sabotage risk elevated.")
