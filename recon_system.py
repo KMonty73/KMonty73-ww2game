1e452ec9bdfe51fdbb2c3bc626b5d740595cc77d
KMonty73 <137738741+KMonty73@users.noreply.github.com>
2026-06-04 07:38:48 -0400

    Create recon_system.py

---
+++ recon_system.py
@@ -0,0 +1,7 @@
+def launch_recon():
+
+    print("\nRECON MISSION")
+    print("-------------------")
+    print("Recon aircraft launched.")
+    print("Recon photos will return next turn.")
+    print("Enemy camouflage may affect results.")
