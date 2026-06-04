3898269d3d9c5c14b3eca86f2a0687e0db5fba59
KMonty73 <137738741+KMonty73@users.noreply.github.com>
2026-06-04 07:49:18 -0400

    Create radio_system.py

---
+++ radio_system.py
@@ -0,0 +1,9 @@
+def radio_intercepts():
+
+    print("\nRADIO INTERCEPTS")
+    print("-------------------")
+    print("Heavy enemy radio chatter detected.")
+    print("Possible armored buildup.")
+    print("High command traffic intercepted.")
+    print("Recon recommended for verification.")
+    print("Signal strength elevated in nearby sectors.")
