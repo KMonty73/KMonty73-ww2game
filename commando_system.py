19d35865a36a1fbc82c45a590d969c348d554c8a
KMonty73 <137738741+KMonty73@users.noreply.github.com>
2026-06-04 07:48:31 -0400

    Create commando_system.py

---
+++ commando_system.py
@@ -0,0 +1,9 @@
+def commando_operation():
+
+    print("\nCOMMANDO OPERATION")
+    print("-------------------")
+    print("Commandos infiltrated enemy territory.")
+    print("Radio silence maintained.")
+    print("Bridge demolition successful.")
+    print("Enemy rail movement disrupted.")
+    print("Extraction underway.")
